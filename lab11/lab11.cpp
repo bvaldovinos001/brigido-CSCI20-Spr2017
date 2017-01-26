3673f18ffa8ccc88556eda04ef0b27ca09b33c7b
Brigido <bvaldovinos001@student.butte.edu>
2017-01-26 23:04:35 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,14 @@
+/* 
+I'm creating a series of instructions for a game between a user and a program 
+The program will randomly generate a number
+The number generated will be a number between two pre-selected numbers, a boundary of numbers.
+The number will be generated randomly when the user commands it.
+The user will guess a number
+Then the user will have the program generate a random number.
+If the user guesses the number correctly, the user wins.
+If the user does not guess correctly, the program will tell them to chose a new number, either "higher" or "lower".
+The uuser will then guess another number according to the feedback from the program.
+The program will continue to tell them to guess again, telling them to go higher or lower until the user guesses correctly.
+The game will keep count of incorrect guesses, deducting 1 point with every incorrect guess.
+The user starts with 10 points.
+*/
