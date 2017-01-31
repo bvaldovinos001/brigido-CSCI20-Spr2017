2e7e6270a36815eef167eeeb3960cc10e53cc3e4
Brigido <bvaldovinos001@student.butte.edu>
2017-01-31 23:08:22 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+cout<<"                /\\/\\    "<<endl;
+cout<<"               / .. )   "<<endl;
+cout<<"            .-',_ y/   "<<endl;
+cout<<"          .'      (     "<<endl;
+cout<<"         /   \\/   |     "<<endl;
+cout<<"     _._|   _/| //     "<<endl;
+cout<<"    ''_ _\\__) \\_))     "<<endl;
+cout<<"    '----'              "<<endl;
+}
