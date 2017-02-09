dbd04ce1567e7dd694d80247cf120e212dd17c72
Brigido <bvaldovinos001@student.butte.edu>
2017-02-09 23:37:55 +0000

    Lab1.5 submission

---
+++ Lab15/lab15.cpp
@@ -0,0 +1,33 @@
+/* Brigido Valdovinos
+   02/09/2017
+   This program is a simple mad lib program that implements at least five variables.
+*/
+ #include <iostream>
+ #include <string>
+ using namespace std;
+ int main() { 
+     string     oneName;
+     string oneAdjective;
+     string     oneNoun;
+     string     oneVerb;
+     string   twoVerb;
+
+cout<<"Insert a character name, make it quick!" <<endl;
+cin>> oneName;
+cout<<"Insert an adjective (describes):" <<endl;
+cin>> oneAdjective;
+cout<< "Insert a noun (place/thing):"<<endl;
+cin>> oneNoun;
+cout<< "Insert a verb (action ending in -ing)"<<endl;
+cin>> oneVerb;
+cout<< "Insert another verb (in past tense)" <<endl;
+cin>> twoVerb;
+
+cout<<"There was once a little boy named "<<oneName<<". ";
+cout<<"He really liked "<<oneVerb <<" in rain puddles and playing in the rain. ";
+cout<<"One "<<oneAdjective<<" day, however, he went outside to happily play in the rain. ";
+cout<<"When he jumped in a puddle, he "<<twoVerb<<".";
+cout<<"The puddle was so deep that he came out "<<oneNoun;
+cout<<"and he never jumped in a puddle again. ";
+
+ }
