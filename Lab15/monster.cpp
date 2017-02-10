f60107d44339c65deda50368226c305941e29f05
Brigido <bvaldovinos001@student.butte.edu>
2017-02-10 00:12:49 +0000

    monster submission

---
+++ Lab15/monster.cpp
@@ -0,0 +1,26 @@
+/* Brigido, Miguel, Daniel, Rosy, Sydney
+02/09/17
+Constructing a monster using struct
+*/
+
+ #include <iostream>
+ #include <string>
+  using namespace std;
+ struct Monster {
+ 
+ string head;
+ string eyes;
+ string ears;
+ string nose;
+ string mouth;
+ 
+ };
+
+ int main() { 
+     Monster Freakystein;
+     Freakystein.head = "Franken";
+     Freakystein.eyes = "Vegitas";
+     Freakystein.ears = "Wackus";
+     Freakystein.nose = "Vegitas";
+     Freakystein.mouth = "Vegitas";
+ }
