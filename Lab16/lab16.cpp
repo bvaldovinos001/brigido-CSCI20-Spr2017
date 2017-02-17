14bb3b84cef238e047ddceae405b6861edc7805b
Brigido <bvaldovinos001@student.butte.edu>
2017-02-17 08:36:46 +0000

    Lab 1.6 sunmission

--- Lab16/lab16.cpp
+++ Lab16/lab16.cpp
@@ -28,7 +28,48 @@
     twoMonster.ears     ;
     twoMonster.mouth  ;
     
+ 
+ // First monster "oneMonster"
+ 
  cout<<"For the first monster, enter a head:";     
  cin>> oneMonster.head ;
- cout<<oneMonster.head ;
+ 
+ cout<< "Enter eyes:";
+ cin>>oneMonster.eyes  ;
+ 
+ cout<< "Enter ears:";
+ cin>>oneMonster.ears  ;
+ 
+ cout<< "Enter mouth:";
+ cin>>oneMonster.mouth  ;
+ 
+ 
+ // Second monster "twoMonster"
+ cout<<"For the second monster, enter a head:";     
+ cin>> twoMonster.head ;
+ 
+ cout<< "Enter eyes:";
+ cin>> twoMonster.eyes  ;
+ 
+ cout<< "Enter ears:";
+ cin>> twoMonster.ears  ;
+ 
+ cout<< "Enter mouth:";
+ cin>> twoMonster.mouth ;
+ cout<<endl;
+ 
+ cout  //First monster output.
+     <<"Monster One: "
+     <<oneMonster.head  <<", "
+     <<oneMonster.eyes  <<", "
+     <<oneMonster.ears  <<", "
+     <<oneMonster.mouth <<". "<<endl;
+     
+ cout  //Second monster output.
+     <<"Monster Two: "
+     <<twoMonster.head  <<", "
+     <<twoMonster.eyes  <<", "
+     <<twoMonster.ears  <<", "
+     <<twoMonster.mouth <<". ";
+ 
  }
