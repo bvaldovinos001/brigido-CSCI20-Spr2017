fd18d2f7f79683074b7724715a492eae8bc98c55
Brigido <bvaldovinos001@student.butte.edu>
2017-03-01 07:56:55 +0000

    Lab2.3 submission

---
+++ Lab23/lab23.cpp
@@ -0,0 +1,32 @@
+/* Brigido Valdovinos
+   02/28/2017
+   This program creates a random number between two chosen limits,
+   the limits being chosen by the user. */
+   
+   #include <iostream>
+   #include <cstdlib>
+   using namespace std;
+   
+   
+   int swapNum(int numX, int numY, int newNumX, newNumY) {
+    int newNumX = numY;
+    int newNumY = numX;
+
+   int RandomNumber (int newNumX, int newNumY) 
+int newNumX = 0;  
+int newNumY = 0; 
+   }
+   
+   
+int main(){
+
+
+
+       cout<< "Enter lower number:"<<endl;
+       cin>> numX;
+       cout<< "Enter upper number:"<<endl;
+       cin>> numY;
+       
+    cout<<((rand()% newNumX - newNumY) + newNumY << endl;
+    
+    }
