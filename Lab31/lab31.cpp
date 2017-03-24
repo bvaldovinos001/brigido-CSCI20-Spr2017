5386bc10ed99347bc96bdf5b4dbd3c11c284416d
Brigido <bvaldovinos001@student.butte.edu>
2017-03-24 06:49:01 +0000

    Lab 3.1 submission

---
+++ Lab31/lab31.cpp
@@ -0,0 +1,25 @@
+/* 
+Brigido Valdovinos
+03/23/2017
+This program is to create a billing formula for ia cell phone company according to how
+their customers data rates and cell phone types are set up. It will use if/else and 
+appropriate conditions      */
+
+ #include <iostream>
+ using namespace std;
+ 
+ 
+ 
+ 
+ int main() {
+
+
+
+/*
+
+I have a math test on friday so I need to study
+for it, I will finish the lab this weekend.
+
+*/
+
+}
