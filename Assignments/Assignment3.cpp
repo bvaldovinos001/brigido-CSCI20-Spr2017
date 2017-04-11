4a87adc1ceb5537c09cf2c3bcf487b5fa29ee90c
Brigido <bvaldovinos001@student.butte.edu>
2017-04-11 19:08:33 +0000

    Assignment 3

--- Assignments/Assignment3.cpp
+++ Assignments/Assignment3.cpp
@@ -45,215 +45,247 @@ int main()
 5 = Spock
             */
 
-while (userChoice <= 5 && compChoice <= 5 && gamesPlayed <= numOfGames) {  
-    //While loop so the choices stay within the limits of the game
-    //and the game can be played a certain amount of times.
-    
-    
-    if (userChoice<=0 && userChoice>=6){
-    cout<< "Enter valid choice: "<<endl;
-    }       
-    
-                //I have this in the loop so that the options are always explained.
-        cout <<endl<<"    Chose an attacker: "<<endl;
-        cout <<" 1 = Rock, 2 = Paper " << endl;
-        cout <<" 3 = Scissors, 4 = Lizard, 5 = Spock " << endl;
+    while (userChoice <= 5 && compChoice <= 5 && gamesPlayed <= numOfGames) {  
+                //While loop so the choices stay within the limits of the game
+                //and the game can be played a certain amount of times.
+                
+            
+                                        if (userChoice<=0 && userChoice>=6){
+                                        cout<< "Enter valid choice: "<<endl;
+                                        }       
+                            
+                                        //I have this in the loop so that the options are always explained.
+                                cout <<endl<<"    Chose an attacker: "<<endl;
+                                cout <<" 1 = Rock, 2 = Paper " << endl;
+                                cout <<" 3 = Scissors, 4 = Lizard, 5 = Spock " << endl;
+                        
+                        
+                        
+                                srand(time(0)); // Random number.
+                                compChoice =((rand() % 5) +1);//Random from 1-5.
+                        
+                    cin>>userChoice;
+                    cout<<endl;
 
 
 
-srand(time(0)); // Random number.
-compChoice =((rand() % 5) +1);//Random from 1-5.
+            if (userChoice == 1) {          // if the user picks rock. 
+                cout << "You chose rock." << endl;
+                
+                        if (compChoice == 1) {
+                            cout << "The computer chose rock." << endl;
+                            cout << "its a tie" << endl;    //If it's a tie, I will not keep track of scores.
+                                }
+                        
+                       else if (compChoice == 2) { 
+                            cout << "The computer chose paper." << endl;
+                            cout << "You lose" << endl;
+                                compWins++;     //Every time I lose, the computer gets a point.
+                                }
+                                
+                        else if (compChoice == 3) {
+                            cout << "The computer chose scissors." << endl;
+                            cout << "You win" << endl;
+                                userWins++;     //Every time I win, I get a point.
+                                }
+                        else if (compChoice == 4) {
+                            cout << "The computer chose lizard." << endl;
+                            cout << "You win" << endl;
+                                 userWins++;
+                                }
+                                
+                        else if (compChoice == 5) {
+                            cout << "The computer chose Spock." << endl;
+                            cout << "You lose" << endl;
+                                compWins++;    
+                                }
+                }
+            
+            
+            
+            
+            
+            else if (userChoice == 2) {      // if the user picks paper
+                cout << "You chose paper." << endl;
+                
+                        if (compChoice == 1) {
+                            cout << "The computer chose rock." << endl;
+                            cout << "You win" << endl;    
+                                userWins++;    
+                                }
+                            
+                        else if (compChoice == 2) {
+                            cout << "The computer chose paper." << endl;
+                            cout << "Its a tie" << endl;
+                                }        
+                            
+                        else if (compChoice == 3) {
+                            cout << "The computer chose scissors." << endl;
+                                cout << "You lose" << endl;
+                                compWins++;    
+                                }    
+                            
+                        else if (compChoice == 4) {
+                            cout << "The computer chose lizard." << endl;
+                            cout << "You lose" << endl;
+                               compWins++;
+                                }
+                            
+                        else if (compChoice == 5) {
+                            cout << "The computer chose Spock." << endl;
+                            cout << "You win" << endl;
+                            userWins++;
+                                }
+                }
+                   
+                   
+                   
+                    
+                    
+            else if (userChoice == 3) {            // if the user picks scissors
+                cout << "You chose scissors." << endl;
+                
+                        if (compChoice == 1) {
+                            cout << "The computer chose rock." << endl;
+                            cout << "You lose" << endl;
+                            compWins++;
+                                }
+                            
+                        else if (compChoice == 2) {
+                            cout << "The computer chose paper." << endl;
+                            cout << "You win" << endl;
+                                userWins++;
+                                }    
+                            
+                        else if (compChoice == 3) {
+                            cout << "The computer chose scissors." << endl;
+                                cout << "Its a tie" << endl;
+                                } 
+                            
+                        else if (compChoice == 4) {
+                            cout << "The computer chose lizard." << endl;
+                                cout << "You win" << endl;
+                                userWins++;
+                                }
+                            
+                        else if (compChoice == 5) {
+                            cout << "The computer chose Spock." << endl;
+                            cout << "You lose" << endl;
+                            compWins++;    
+                                }
+                            
+                }
+            
+            
+            
+            
+            
+            else if (userChoice == 4) {                  // if the user picks lizard
+                cout << "You chose lizard." << endl;
+                
+                        if (compChoice == 1) {
+                            cout << "The computer chose rock." << endl;
+                            cout << "You lose" << endl;
+                            compWins++;
+                                }
+                            
+                        else if (compChoice == 2) {
+                            cout << "The computer chose paper." << endl;
+                            cout << "You win" << endl;
+                            userWins++;
+                                }
+                            
+                        else if (compChoice == 3) {
+                            cout << "The computer chose scissors." << endl;
+                            cout << "You lose" << endl;
+                            compWins++;
+                                }
+                            
+                        else if (compChoice == 4) {
+                            cout << "The computer chose lizard." << endl;
+                            cout << "Its a tie" << endl;
+                                }    
+                            
+                        else if (compChoice == 5) {
+                            cout << "The computer chose Spock." << endl;
+                            cout << "You win" << endl;
+                            userWins++;
+                                }
+                
+            }
+            
+            
+            
+            
+            
+            else if (userChoice == 5) {                  // if the user picks spock
+                cout << "You chose Spock." << endl;
+                
+                        if (compChoice == 1) {
+                            cout << "The computer chose rock." << endl;
+                            cout << "You win" << endl;
+                            userWins++;
+                                }
+                            
+                        else if (compChoice == 2) {
+                            cout << "The computer chose paper." << endl;
+                            cout << "You lose" << endl;
+                            compWins++;
+                                }    
+                        
+                        else if (compChoice == 3) {
+                            cout << "The computer chose scissors." << endl;
+                            cout << "You win" << endl;    
+                            userWins++;
+                                }    
+                            
+                        else if (compChoice == 4) {
+                            cout << "The computer chose lizard." << endl;
+                            cout <<  "You lose" << endl;
+                            compWins++;
+                                }
+                        
+                        else if (compChoice == 5) {
+                            cout << "The computer chose Spock." << endl;
+                            cout << "Its a tie" << endl;
+                                }    
+            
+            }
+            
+            
+            
+            
+            
 
-cin>>userChoice;
-cout<<endl;
+        gamesPlayed = gamesPlayed + 1;
 
-
-if (userChoice == 1) {          // if the user picks rock. 
-    cout << "You chose rock." << endl;
-    
-if (compChoice == 1) {
-    cout << "The computer chose rock." << endl;
-    cout << "its a tie" << endl;    //If it's a tie, I will not keep track of scores.
 }
 
-if (compChoice == 2) { 
-    cout << "The computer chose paper." << endl;
-    cout << "You lose" << endl;
-        compWins++;     //Every time I lose, the computer gets a point.
-    }
-if (compChoice == 3) {
-    cout << "The computer chose scissors." << endl;
-    cout << "You win" << endl;
-        userWins++;     //Every time I win, I get a point.
-    }
-if (compChoice == 4) {
-    cout << "The computer chose lizard." << endl;
-    cout << "You win" << endl;
-         userWins++;
-        
-    }
-if (compChoice == 5) {
-    cout << "The computer chose Spock." << endl;
-    cout << "You lose" << endl;
-        compWins++;    
-    }
-}
-if (userChoice == 2) {      // if the user picks paper
-    cout << "You chose paper." << endl;
-    
-if (compChoice == 1) {
-    cout << "The computer chose rock." << endl;
-    cout << "You win" << endl;    
-        userWins++;    
-    }
-    
-if (compChoice == 2) {
-    cout << "The computer chose paper." << endl;
-    cout << "Its a tie" << endl;
-    }        
-    
-if (compChoice == 3) {
-    cout << "The computer chose scissors." << endl;
-        cout << "You lose" << endl;
-        compWins++;
-        
-    }    
-    
-if (compChoice == 4) {
-    cout << "The computer chose lizard." << endl;
-    cout << "You lose" << endl;
-       compWins++;
-    }
-    
-if (compChoice == 5) {
-    cout << "The computer chose Spock." << endl;
-    cout << "You win" << endl;
-    userWins++;
-    }
-}
-if (userChoice == 3) {            // if the user picks scissors
-    cout << "You chose scissors." << endl;
-    
-if (compChoice == 1) {
-    cout << "The computer chose rock." << endl;
-    cout << "You lose" << endl;
-    compWins++;
-    }
-    
-if (compChoice == 2) {
-    cout << "The computer chose paper." << endl;
-    cout << "You win" << endl;
-        userWins++;
-        
-    }    
-    
-if (compChoice == 3) {
-    cout << "The computer chose scissors." << endl;
-        cout << "Its a tie" << endl;
-    } 
-    
-if (compChoice == 4) {
-    cout << "The computer chose lizard." << endl;
-        cout << "You win" << endl;
-        userWins++;
-    }
-    
-if (compChoice == 5) {
-    cout << "The computer chose Spock." << endl;
-    cout << "You lose" << endl;
-    compWins++;    
-        
-    }
-}
-if (userChoice == 4) {                  // if the user picks lizard
-    cout << "You chose lizard." << endl;
-    
-if (compChoice == 1) {
-    cout << "The computer chose rock." << endl;
-    cout << "You lose" << endl;
-    compWins++;
-        
-    }
-    
-if (compChoice == 2) {
-    cout << "The computer chose paper." << endl;
-    cout << "You win" << endl;
-    userWins++;
-        
-    }
-    
-if (compChoice == 3) {
-    cout << "The computer chose scissors." << endl;
-    cout << "You lose" << endl;
-    compWins++;
-        
-    }
-    
-if (compChoice == 4) {
-    cout << "The computer chose lizard." << endl;
-    cout << "Its a tie" << endl;
-    }    
-    
-if (compChoice == 5) {
-    cout << "The computer chose Spock." << endl;
-    cout << "You win" << endl;
-    userWins++;
-        
-    }
-    
-}    
-if (userChoice == 5) {                  // if the user picks spock
-    cout << "You chose Spock." << endl;
-    
-if (compChoice == 1) {
-    cout << "The computer chose rock." << endl;
-    cout << "You win" << endl;
-    userWins++;
-        
-    }
-    
-if (compChoice == 2) {
-    cout << "The computer chose paper." << endl;
-    cout << "You lose" << endl;
-    compWins++;
-    }    
 
-if (compChoice == 3) {
-    cout << "The computer chose scissors." << endl;
-    cout << "You win" << endl;    
-    userWins++;
-    }    
-    
-if (compChoice == 4) {
-    cout << "The computer chose lizard." << endl;
-    cout <<  "You lose" << endl;
-    compWins++;
-    }
-
-if (compChoice == 5) {
-    cout << "The computer chose Spock." << endl;
-    cout << "Its a tie" << endl;
-}    
-
-}
-
-gamesPlayed = gamesPlayed + 1;
-
-}
-
-Ties = (5 - userWins - compWins);   //Wanted to keep track of ties.
+        Ties = (numOfGames - userWins - compWins);   //Wanted to keep track of ties.
 
 //This will display the scores of  user and computer, and ties.
-cout<<endl<<"User score: "<<userWins<<endl;
-cout<<"Computer score: "<<compWins<<endl;
-cout<<"Ties: "<< Ties<<endl;
-if (userWins > compWins){
-    cout<< "You have won!"<<endl;   //If I win.
-}
-else if (userWins < compWins){
-    cout<<"You have lost."<<endl;   //If computer wins.
-}
-else{
-    cout<<"You have tied with the computer"<<endl;  //If I tie.
-}
+    cout<<endl<<"User score: "<<userWins<<endl;
+    
+    cout<<"Computer score: "<<compWins<<endl;
+    
+    cout<<"Ties: "<< Ties<<endl;
+    
+        
+        
+        
+        
+        if (userWins > compWins){
+            cout<< "You have won!"<<endl;   //If I win.
+                }
+        
+        
+        else if (userWins < compWins){
+            cout<<"You have lost."<<endl;   //If computer wins.
+                }
+        
+                
+        else{
+            cout<<"You have tied with the computer"<<endl;  //If I tie.
+                }
 }
