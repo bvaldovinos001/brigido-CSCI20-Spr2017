ebaf9174b7558b465363b7605c62c3da42ae2208
Brigido <bvaldovinos001@student.butte.edu>
2017-04-14 06:47:49 +0000

    Lab 4.2 submission

---
+++ Lab42/lab42.cpp
@@ -0,0 +1,23 @@
+/*
+Brigido Valdovinos
+04/13/17
+This program is a simulator for a store inventory managment system. 
+It keeps track of inventory, purchases, price.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+
+//Didn't have enough time to get started, I will finish it this weekend.
+
+
+
+
+
+
+}
