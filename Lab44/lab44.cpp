7078bb4c716d1a615f5050a8b77e4550a0118526
Brigido <bvaldovinos001@student.butte.edu>
2017-04-21 08:02:10 +0000

    Lab 4.4 submission

--- Lab44/lab44.cpp
+++ Lab44/lab44.cpp
@@ -7,42 +7,64 @@ if the user name is taken.
 */
 
 #include <iostream>
-#include <cstring>
+#include <cstring>  //Enables use of string functions.
+#include <cctype>
 using namespace std;
 
 
 int main()
 {
 
-
-
 char firstName[10];
 char lastName [20];
 
 
+    cout<< "Enter first name: ";
+    cin>> firstName;
+              
+            while (strlen(firstName) > 10) //If name is longer than 10 characters.
+                {
+                    cout<< "Too many characters, type less than 10."<<endl;
+                    cin>> firstName;
+                }
+
+
+    cout<<"Enter last name: ";
+    cin>> lastName;  
+
+            while (strlen(lastName) > 20) //If last name is longer than 20 characters.
+                {
+                    cout<< "Too many characters, type less than 10."<<endl;
+                    cin>> lastName;
+                }
+
+
+            if( strcmp(firstName,lastName) == 0 ) //Checking if names are the same.
+                {
+                    cout<< "First and last name are the same, re-enter last name: "<<endl;
+                    cin>>lastName;
+                }
 
 
 
-cout<< "Enter your first name: "<<endl;
-cin>> firstName;
 
 
-while (strlen(firstName) > 10) 
-        {
-        
-            cout<< "Too many characters, type less than 10."<<endl;
-            cin>> firstName;
-        }
+cout<<endl << "Name: "<<firstName<<" "<<lastName<<endl;
+cout<< "Which user name would you like? "<<endl;
 
-cout<<"Enter last name: ";
-cin>> lastName;
-while (strlen(lastName) > 20) 
-        {
-        
-            cout<< "Too many characters, type less than 10."<<endl;
-            cin>> lastName;
-        }
-
-
-
+cout<< firstName[0] << lastName << endl;
+cout<< firstName[0] << firstName[1] << lastName << endl;
+cout<< firstName << lastName << endl;    
 }
+
+
+
+/*
+    Trying to change the character from upper to lower case.
+int i = 0;
+char c;
+
+c=firstName[i];
+    putchar (tolower(c));
+
+*/
