373f864376b02c5455e980271c2c3ff996c42d9b
Brigido <bvaldovinos001@student.butte.edu>
2017-05-10 06:26:12 +0000

    Lab 5.2 submission

---
+++ Lab52/lab21.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "main.h"
+using namespace std;
+
+// Program starts here
+int main() {
+  // Create a MyClass Object
+  MyClass object;
+
+  //Send a number to the Object
+  object.SetNumber(randomNumber());
+
+  // Call the output member function
+  object.Output();
+
+  // This ends our program
+  return 0;
+}
---
+++ Lab52/main.h
@@ -0,0 +1,11 @@
+#include <ctime>
+#include <cstdlib>
+#include "MyClass.h"
+
+
+
+
+int randomNumber(){
+   srand(time(0));
+   return (rand()% 10) + 1;
+}
